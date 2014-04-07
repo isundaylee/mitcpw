100638fad521b7aaf77ba060dcdd3a85b5d1eb74
Jiahao Li <isundaylee.reg@gmail.com>
2014-04-06 22:23:00 -0400

    Adds whole calendar generation.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,29 @@ class EventsController < ApplicationController
 
       @events = @filtered.paginate(page: params[:page])
     end
+
+    respond_to do |format|
+      format.html
+      format.ics do
+        require 'icalendar'
+
+        cal = Icalendar::Calendar.new
+
+        Event.all.each do |e|
+          cal.event do
+            dtstart DateTime.parse(e.from.localtime.to_s)
+            dtend DateTime.parse(e.to.localtime.to_s)
+            location e.location
+            summary e.title
+            description e.summary
+          end
+        end
+
+        cal.publish
+
+        @ics = cal.to_ical
+      end
+    end
   end
 
   def show
